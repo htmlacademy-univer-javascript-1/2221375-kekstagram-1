af04561c09df330c935e2ee3df9a4ccf53844b0d
Александр1~ <adventchiken@gmail.com>
2022-12-21 03:13:18 +0500

    Надо подкачаться

--- js/consts.js
+++ js/consts.js
@@ -1,30 +1,9 @@
-const NAMES = ['Глеб Голубин', 'Гончаров Илья', 'Владимиров Николай', 'Щербаков Иван', 'Климов Роман', 'Самсонов Даниэль', 'Федоров Роман', 'Ефремов Даниил', 'Старостин Максим'];
-
-const MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-
-const DESCRIPTIONS = ['Улыбка на миллион','Хорошее фото','Коммутатор'];
-
-const AvatarNumber = {
-  MIN: 1,
-  MAX: 6
-};
-
-const CountLike = {
-  MIN: 15,
-  MAX: 200
-};
-
-const CountComment = {
-  MIN: 0,
-  MAX: 23
-};
-
 const DEFAULT_RENDERED_COMMENTS = 5;
-const MAX_COUNT_PHOTOS = 25;
 const STEP_ADDED_COMMENTS = 5;
 const MAX_STRING_LENGTH = 140;
 const MAX_COUNT_HASHTAG = 5;
 const MAX_HASHTAG_LENGTH = 20;
+const ALERT_SHOW_TIME = 3000;
 
 const MessageError = {
   START_WITH: 'Хэш-тег должен начинаться с символа #',
@@ -37,4 +16,4 @@ const MessageError = {
   MAX_LENGTH_COMMENT: `Максимальная длина комментария ${MAX_STRING_LENGTH} символов`
 };
 
-export { NAMES, MESSAGES, DESCRIPTIONS, AvatarNumber, CountLike, CountComment, DEFAULT_RENDERED_COMMENTS, MAX_COUNT_PHOTOS, STEP_ADDED_COMMENTS, MAX_STRING_LENGTH, MAX_COUNT_HASHTAG, MAX_HASHTAG_LENGTH, MessageError };
+export { DEFAULT_RENDERED_COMMENTS, STEP_ADDED_COMMENTS, MAX_STRING_LENGTH, MAX_COUNT_HASHTAG, MAX_HASHTAG_LENGTH, ALERT_SHOW_TIME, MessageError };
--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-import { createPhotos } from './data.js';
 import { initThumbnails } from './thumbnails.js';
 import { renderUploadForm } from './form.js';
-import { initEffects } from './filters.js';
+import {getData} from './api.js';
+import { showAlert } from './utils.js';
+import { ALERT_SHOW_TIME } from './consts.js';
 
-const data = createPhotos();
+getData(
+  (photos) => initThumbnails(photos),
+  () => showAlert('Не удалось загрузить фотографии, обновите страницу', ALERT_SHOW_TIME),
+);
 
-initThumbnails(data);
-renderUploadForm(data);
-initEffects();
+renderUploadForm();
